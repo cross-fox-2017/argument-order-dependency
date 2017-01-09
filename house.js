5d2f0c7352d08457067a14e3bde09d2f68f3d700
isumizumi <isumi.karina72@gmail.com>
2017-01-09 11:28:40 +0700

    Finish

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(property) {
+    this.address = property["address"]
+    this.square_feet = property["square_feet"]
+    this.num_bedrooms = property["num_bedrooms"] || 3
+    this.num_baths = property["num_baths"] || 2
+    this.cost = property["cost"] || 320000
+    this.down_payment = property["down_payment"] || 0.20
+    this.sold = property["sold"] || false
+    this.short_sale = property["short_sale"]
+    this.has_tenants = property["has_tenants"] || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -28,6 +28,9 @@ class House {
   }
 
   to_s() {
-    return `${obscure_address} : ${square_feet} sq. ft., ${num_bedrooms} bed, ${num_baths} bath. ${cost}`
+    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath., ${this.cost}`
   }
 }
+
+var komplek = new House ({address : "Jl.Kaya Raya No.1", square_feet : 100})
+console.log(komplek.to_s())
