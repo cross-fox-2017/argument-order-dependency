abc6474c540a494d308955c934e775651fb3c949
mfpebrizal <ftm.pebrizal@gmail.com>
2017-01-09 11:25:00 +0700

    argument-order-dependency mfpebrizal

--- house.js
+++ house.js
@@ -2,16 +2,27 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  // constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, has_tenants)
+  constructor(components) {
+    this.address = components['address']
+    this.square_feet = components['square_feet']
+    this.num_bedrooms = components[num_bedrooms] || 3
+    this.num_baths = components['num_baths'] || 2
+    this.cost = components['cost'] || 320000
+    this.down_payment = components['down_payment'] || 0.20
+    this.sold = components['sold'] || false
+    this.short_sale = components[coshort_sale]
+    this.has_tenants = components[has_tenants] || false
+
+    // this.address = address
+    // this.square_feet = square_feet
+    // this.num_bedrooms = num_bedrooms || 3
+    // this.num_baths = num_baths || 2
+    // this.cost = cost || 320000
+    // this.down_payment = down_payment || 0.20
+    // this.sold = sold || false
+    // this.short_sale = short_sale
+    // this.has_tenants = has_tenants || false
   }
 
   obscure_address() {
